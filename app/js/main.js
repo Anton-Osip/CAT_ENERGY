98f570ac87e51b5e2403e950e65db6c10106226e
Anton-Osip <osipchika@gmail.com>
2022-11-18 16:16:21 +0300

    Написал header с адаптивом

--- app/js/main.js
+++ app/js/main.js
@@ -1,2 +1,10 @@
-import {testFunction} from './scripts/test';
-testFunction();
+const header = document.querySelector(".header");
+const burger = document.querySelector(".logo__burger");
+
+burger.addEventListener("click", () => {
+  header.classList.toggle("header--active");
+});
+
+if (header.classList.contains("header-no-js")) {
+  header.classList.remove("header-no-js");
+}
